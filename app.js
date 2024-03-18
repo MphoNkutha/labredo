0a9c26daeaa383f2363cb6ec0335bc80e247ae29
MphoNkutha <160428662+MphoNkutha@users.noreply.github.com>
2024-03-18 13:01:46 +0000

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express ();
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log("Server Listening on PORT:", port);
+  console.log("Server Listening on PORT:", PORT);
 });
 
 app.get("/status", (request, response) => {
